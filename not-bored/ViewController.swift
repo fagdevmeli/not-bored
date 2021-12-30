fa7fcab9f6bb8e4f81d5553b1412e18cdb5b45f9
fagdevmeli <flavio.gomez@mercadolibre.com>
2021-12-30 09:49:24 -0300

    Add files via upload
    first commit

---
+++ not-bored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  not-bored
+//
+//  Created by Flavio Andres Gomez on 29/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
